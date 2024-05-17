10e6dcea896c366012517eae20537e27751bc2a2
nikita <nikitabajaj790@gmail.com>
2024-05-17 10:57:43 +0530

    handled ResourceNotFoundException

---
+++ blog-app-apis/src/main/java/com/blogapp/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.blogapp.exceptions;
+
+//this is a global exception class used for handling exceptions across all controller
+
+import com.blogapp.payloads.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ApiResponse> resourceNotFoundExceptionHandler(ResourceNotFoundException e){
+        String message = e.getMessage();
+        ApiResponse apiResponse = new ApiResponse(message, false);
+        return new ResponseEntity<>(apiResponse, HttpStatus.NOT_FOUND);
+    }
+
+}
